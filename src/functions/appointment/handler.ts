2b545bcf31a5f3e15f8d93d80f48f2d460a65a71
Jesus Marenco <jesus.marenco@externos-cl.cencosud.com>
2022-07-28 19:22:46 -0500

    Lambda conectada a la principal

--- src/functions/appointment/handler.ts
+++ src/functions/appointment/handler.ts
@@ -1,10 +1,9 @@
 
 export const appointmentHandler = async (event) => {
-  const body = event.body;
   console.log("estoy desde la lambda que ejecutara operaciones desde colombia");
 
   return {
     statusCode: 200,
-    body
+    event
   };
 };
