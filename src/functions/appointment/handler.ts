f6e3a4c281bfee21ad2d84750cf4d9dc067a77e5
Jesus Marenco <jesus.marenco@externos-cl.cencosud.com>
2022-08-09 14:23:09 -0500

    update status_appointment to 1 in dynamodb aws

--- src/functions/appointment/handler.ts
+++ src/functions/appointment/handler.ts
@@ -1,7 +1,23 @@
+import * as AWS from "aws-sdk";
 
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 export const appointmentHandler = async (event) => {
   console.log("estoy desde la lambda que ejecutara operaciones desde colombia");
   console.log(event);
+  const id = event.detail.id;
+
+  const result = await dynamodb.update({
+    TableName: "Appointment-dev",
+    UpdateExpression: "set status_appointment=:status_appointment",
+    ExpressionAttributeValues: {
+      ":status_appointment": 1
+    },
+    Key: { id },
+    ReturnValues: "ALL_NEW"
+
+  }).promise();
+
+  console.log(result);
 
   return {
     statusCode: 200,
