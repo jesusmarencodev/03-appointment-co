44cbcf3274c8935170321ee03cebd5be7a367e09
Jesus Marenco <jesus.marenco@externos-cl.cencosud.com>
2022-08-12 14:22:10 -0500

    conectando app-gateway con event-bus con sqs de cada pais, con lambda de cada pais, con sqsDLQ para mensajes fallidos

--- src/functions/appointment/index.ts
+++ src/functions/appointment/index.ts
@@ -4,14 +4,17 @@ export default {
   handler: `${getPathHandler(__dirname)}/handler.appointmentHandler`,
   events :[
     {
-      eventBridge : {
+      sqs: {
+        arn: "${ssm:/digital/sqs-co-arn-${self:provider.stage}}",
+      }
+/*       eventBridge : {
         //colocamos el arn del eventBridge que ya tenemsos creado su nombre es EventBusPracticaAWSServerless01
         eventBus: "arn:aws:events:us-east-1:867855303039:event-bus/EventBusPracticaAWSServerless01",
         pattern:{
           source:["appointment"],
           "detail-type": ["appointment-create-co"]
         }
-      }
+      } */
     }
   ]
 };
